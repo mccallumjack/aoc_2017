809380d84a9575b28e22e92c1eeb2d2b65dbcd5f
Jack McCallum <mccallumjack@gmail.com>
2017-12-05 16:41:56 +1100

    Day 5 done

---
+++ elixir/day_5/part_2.exs
@@ -0,0 +1,22 @@
+defmodule Runner do
+  def run do
+    "input.txt" |> read_into_int_lines |> advance
+  end
+
+  def read_into_int_lines(input), do: File.read!(input) |> String.split("\n", trim: true) |> Enum.map(&String.to_integer/1)
+
+  def advance(list), do: advance(list, 0, 0)
+  def advance(list, index, steps) when index >= length(list), do: steps
+  def advance(list, index, steps) do 
+    val = Enum.at list, index
+
+    new_offset = cond do
+      val > 2 -> val - 1
+      true    -> val + 1
+    end
+
+    advance(List.replace_at(list, index, new_offset), index + val, steps + 1)
+  end
+end
+
+IO.puts(Runner.run)
